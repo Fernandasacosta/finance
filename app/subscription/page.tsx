c02fddbbc13e35fea00272619e4fd2a057fb9ed8
Fernandasacosta <fernandasacostaa@gmail.com>
2024-11-07 14:49:47 -0300

    feat: add login page

---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const SubscriptionPage = () => {
+  return <h1>s</h1>;
+};
+
+export default SubscriptionPage;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const TransactionPage = () => {
+  return <h1>su</h1>;
+};
+
+export default TransactionPage;
